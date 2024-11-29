827dc429b27a3ed0d20c67ccb0830e593db3ee2e
Emerson Ferreira <emecatarino@yahoo.com.br>
2024-11-29 11:27:48 -0300

    Correcao do nome do teste de funcionalidade

--- tests/Feature/App/Models/Pessoa/PessoaFuncionalidadesTest.php
+++ tests/Feature/App/Models/Pessoa/PessoaFuncionalidadesTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class PessoaFuncionalidadesTeste extends TestCase
+class PessoaFuncionalidadesTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
