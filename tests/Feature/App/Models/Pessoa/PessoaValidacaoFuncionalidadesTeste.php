75f9234dbd5ff47b08cdc89d2938bbd62c7d6983
Emerson Ferreira <emecatarino@yahoo.com.br>
2024-12-02 11:38:53 -0300

    Teste de campos validos ao criar uma pessoa atraves da rota pessoa.store

---
+++ tests/Feature/App/Models/Pessoa/PessoaValidacaoFuncionalidadesTeste.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\App\Models\Pessoa;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+
+class PessoaValidacaoFuncionalidadesTeste extends TestCase
+{
+    use RefreshDatabase, WithFaker, WithoutMiddleware;
+
+    public function test_campos_obrigatorios_nao_fornecidos()
+    {
+        // Dados incompletos, omitindo campos obrigatórios
+        $dadosIncompletos = [
+            // 'status_id' está ausente
+            'nome' => '', // Campo obrigatório vazio
+            //'email' => '',
+            // Outros campos podem estar presentes ou ausentes, conforme necessário
+        ];
+
+        // Enviar requisição para criar uma pessoa
+        $response = $this->post(route('pessoas.store'), $dadosIncompletos);
+
+        // Verificar se a resposta HTTP é 302, indicando redirecionamento devido a falha de validação
+        // $response->assertStatus(302);
+
+        // Verificar se os erros de validação são retornados para os campos obrigatórios
+        $response->assertSessionHasErrors([
+            'status_id',
+            'nome',
+            'email',
+        ]);
+    }
+
+}
